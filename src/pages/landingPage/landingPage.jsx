12972aa991f6fa537c1268c8d8312d7648b2d8c8
G00KU <gookul18@hotmail.com>
2023-10-17 09:23:19 +0530

    removed all the existing files and added falling stars components

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -1,20 +1,37 @@
-import { FlexContainer, ImageHolder, TitleContainer, logo } from "../../allExports";
-const logoContainerStyles ={
-    width:"35rem",
-    height:"35rem",
+import { useEffect, useState } from 'react';
+import star from '../../images/star.png';
+
+export const LandingPage = () => {
+    const [starCount, setStarCount] = useState([]);
+    useEffect(() => {
+        const newStarCount = [];
+        for (let i = 0; i < 40; i++) {
+            newStarCount.push(i);
+        }
+        setStarCount(newStarCount);
+    }, []);
+    console.log(starCount, 'starCount   ');
+    return (<div id="lander">
+        <div className='fallingStars'>
+            {starCount.map(i => <FallingStars element={star} key={i} />)}
+        </div>
+    </div >);
 }
-const tittleTextStyles={
-    "font-size":'100px',
+
+const FallingStars = ({ element }) => {
+    const startY = 0;
+    const starOrigin = Math.random() * 1000;
+    const startingPt = window.innerWidth < starOrigin ? starOrigin : Math.random() * 1000;
+    const endY = window.innerHeight - startingPt;
+    const styles = {
+        "--elementPosition": Math.random() * 100 + "vw",
+        "--animationDuration": Math.random() * (12 - 17) + 12 + 's',
+        "--animationDelay": (Math.random() * 3 + 1).toFixed(2) + 's',
+        "--elementOrgin": startingPt + "px",
+        "--startY": `${startY}px`,
+        "--endY": `${endY}px`,
+    }
+    return <>
+        <img className="falling-element" style={styles} src={element} />
+    </>
 }
-
-export const LandingPage=()=>{
-
-
-    return (<>
-    <FlexContainer>
-    <ImageHolder image={logo()} styles={logoContainerStyles} className='logoImage'/>
-    <TitleContainer text={"SUPRISE JOURNEY"} textStyles={tittleTextStyles}/>
-    </FlexContainer>
-    </>);
-}
-
